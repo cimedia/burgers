a7a273ad0452af527b57092f9620e28710c74448
Kamil Kierzkowski <biuro@cimedia.pl>
2018-04-17 22:31:50 +0200

    Navigation on mobile and desktop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../hoc/Auxiliary'
 import styles from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    };
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        })
+    };
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClick={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+};
+
+export default Layout;
